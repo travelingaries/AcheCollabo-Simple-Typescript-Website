97c95c3ec5857c3a362a20eae356dabeed6937df
aries <aries@watcha.com>
2022-05-31 15:29:23 +0900

    port -> PORT

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,7 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 // Start the server
-const port = Number(process.env.PORT || 3000);
-app.listen(port, () => {
-//    logger.info('Express server started on port: ' + port);
+const PORT = Number(process.env.PORT || 3000);
+app.listen(PORT, () => {
+//    logger.info('Express server started on port: ' + PORT);
 });
